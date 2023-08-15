7037250be615a33eda85de65765402917e10512c
Peter Holló <hello@songsforthe.dev>
2023-08-15 14:37:05 +0200

    submit content

--- app/components/RTE.client.tsx
+++ app/components/RTE.client.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-function RTE() {
+export type RTEProps = {
+  editorRef: React.MutableRefObject<ReactQuill | null>;
+};
+
+function RTE(props: RTEProps) {
+  const { editorRef } = props;
+
   const limit = 100;
+
+  const toolbar = "toolbar_id";
 
   const [value, setValue] = React.useState("");
   const [length, setLength] = React.useState(0);
-  const ref = React.useRef<ReactQuill | null>(null);
 
   React.useEffect(() => {
-    if (ref !== null && ref.current !== null) {
-      const editor = ref.current.getEditor();
+    if (editorRef !== null && editorRef.current !== null) {
+      const editor = editorRef.current.getEditor();
       const handleTextChange = () => {
         const newLength = editor.getLength() - 1;
 
@@ -22,10 +29,50 @@ function RTE() {
       };
       editor.on("text-change", handleTextChange);
     }
-  }, [ref]);
+  }, [editorRef]);
+
   return (
     <>
-      <ReactQuill theme="snow" value={value} onChange={setValue} ref={ref} />
+      <div id={`${toolbar}`}>
+        <div className="ql-formats">
+          <select className="ql-header">
+            <option value="2">Überschrift 1</option>
+            <option value="3">Überschrift 2</option>
+            <option value="4">Überschrift 3</option>
+            <option value="" selected>
+              Text
+            </option>
+          </select>
+        </div>
+        <div className="ql-formats">
+          <button className="ql-bold">fett</button>
+          <button className="ql-italic">kursiv</button>
+          <button className="ql-underline">unterstrichen</button>
+        </div>
+        <div className="ql-formats">
+          <button type="button" className="ql-list" value="ordered">
+            nummerierte Liste
+          </button>
+          <button type="button" className="ql-list" value="bullet">
+            unnummerierte Liste
+          </button>
+        </div>
+        <div className="ql-formats">
+          <button type="button" className="ql-link">
+            Link
+          </button>
+        </div>
+        <div className="ql-formats">
+          <button className="ql-clean">Format entfernen</button>
+        </div>
+      </div>
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        ref={props.editorRef}
+        modules={{ toolbar: `#${toolbar}` }}
+      />
       <p>
         {length}/{limit} Characters
       </p>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,9 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { ActionArgs, V2_MetaFunction } from "@remix-run/node";
 import RTE from "~/components/RTE.client";
 import { ClientOnly } from "remix-utils";
+import { Form, useSubmit } from "@remix-run/react";
+import React from "react";
+import type ReactQuill from "react-quill";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -9,6 +12,40 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+export const action = async (args: ActionArgs) => {
+  const { request } = args;
+
+  const clone = request.clone();
+
+  const formData = await clone.formData();
+  const text = formData.get("text");
+  const textRTE = formData.get("textRTE");
+
+  console.log({ text, textRTE });
+  return null;
+};
+
 export default function Index() {
-  return <ClientOnly>{() => <RTE />}</ClientOnly>;
+  const editorRef = React.useRef<ReactQuill>(null);
+  const submit = useSubmit();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (editorRef.current !== null) {
+      const editor = editorRef.current.getEditor();
+
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+      formData.set("text", editor.getText());
+      formData.set("textRTE", editor.root.innerHTML);
+      submit(formData, { method: "post" });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <ClientOnly>{() => <RTE editorRef={editorRef} />}</ClientOnly>
+      <button type="submit">Submit</button>
+    </Form>
+  );
 }
